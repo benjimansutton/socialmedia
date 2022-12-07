f721e3e6fbec74823c128eba131622e95dd3a72f
benjimansutton <benjimansutton@gmail.com>
2022-12-07 12:56:59 +0000

    users in controllers update

--- Social Media Website/server/index.js
+++ Social Media Website/server/index.js
@@ -10,7 +10,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -49,6 +50,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 
 /* MONGOOSE SETUP */
